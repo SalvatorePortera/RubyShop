3247bd3d6a8c685f07dfa1c7b48b7b619711f265
SalvatorePortera <salvap0rtera1@gmail.com>
2022-07-07 05:55:59 -0400

    final

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,10 +20,8 @@
 
 $(document).ready(function(){
 	// scroll body to 0px on click
-    $('#back-to-top').click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 400);
-        return false;
+    $('button#backBtn').on('click tap touchstart', function(e){
+        e.preventDefault();
+        window.history.back();
     });
 });
